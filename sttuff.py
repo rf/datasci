0963580f06533f3557e2ec8cb9ba9b2bf4d66801
Russ Frank <me@russfrank.us>
2013-10-01 16:06:48 -0400

    length plot

--- sttuff.py
+++ sttuff.py
@@ -1,10 +1,13 @@
 from __future__ import division
 import csv
+from scipy import stats
 
 rows = []
 
-for row in csv.reader(open("laptops.csv", "rb")) :
+for row in csv.reader(open("laptops.csv", "rb")):
   rows.append(row)
+
+rows = rows[1:]
 
 total_realnamers = 0
 realnamers_verified = 0
@@ -28,3 +31,29 @@ for row in rows:
 
 print "verified realnamers purchase", verified_realnamers / total_verified
 
+release_to_review_times = []
+
+for row in rows:
+  release_to_review_times.append(int(row[1]))
+
+#qs = stats.scoreatpercentile(release_to_review_times, [0, 25, 50, 75, 100])
+print "min &", stats.scoreatpercentile(release_to_review_times, 0), "\\\\"
+print "Q1 &", stats.scoreatpercentile(release_to_review_times, 25), "\\\\"
+print "median &", stats.scoreatpercentile(release_to_review_times, 50), "\\\\"
+print "Q3 &", stats.scoreatpercentile(release_to_review_times, 75), "\\\\"
+print "max &", stats.scoreatpercentile(release_to_review_times, 100), "\\\\"
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+lengths = []
+for row in rows:
+  lengths.append(int(row[7]))
+
+plt.ioff()
+
+thing = plt.hist(lengths, 200)
+plt.savefig("length_plot.png")
+plt.close()
