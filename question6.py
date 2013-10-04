263017c20bfb0753bf82338e5f57e635a189c47b
Russ Frank <russ.frank@rutgers.edu>
2013-10-04 16:45:16 -0400

    work

--- question6.py
+++ question6.py
@@ -1,7 +1,9 @@
-s = [[2, 5],
-    [0, 1],
-    [3, 4],
-    [0, 2, 4]]
+s = [
+  [0, 0, 1, 0, 0, 1],
+  [1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 0],
+  [1, 0, 1, 0, 1, 0]
+]
 
 def h1 (x):
   return (2*x + 1) % 6
@@ -12,4 +14,25 @@ def h2 (x):
 def h3 (x):
   return (5*x + 2) % 6
 
+permutations = [[],[],[]];
 
+for i in range(0, 6):
+  permutations[0].append(h1(i))
+
+for i in range(0, 6):
+  permutations[1].append(h2(i))
+
+for i in range(0, 6):
+  permutations[2].append(h3(i))
+
+def minhash (permutation, document):
+  for ii in permutations[permutation]:
+    if s[document][ii] == 1:
+      return ii
+
+print permutations
+
+print
+
+for i in range(0, 4):
+  print "Signature of document", i+1, ":", minhash(0, i), minhash(1, i), minhash(2, i)
